05eb88698ccf9284fb48c130d99ec633f2945ee1
Josh Arkane Orbiso <orbisojo@gmail.com>
2023-09-09 15:27:57 +0800

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import './templates/Header'
+import './templates/Footer'
+import Header from './templates/Header';
+import Footer from './templates/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col h-screen justify-between">
+      <Header></Header>
+        
+      <Footer></Footer>
     </div>
   );
 }
